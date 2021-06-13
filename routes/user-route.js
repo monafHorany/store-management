7590c8a81d24b7b89894d6154e8bc4bf88ed5869
monaf horany <monafhorany@gmail.com>
2021-06-14 00:58:26 +0300

    edit admin role

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controller/user");
-const { protect, admin, adminANDeditor } = require("../middleware/authMiddleware");
+const {
+  protect,
+  admin,
+  adminANDeditor,
+} = require("../middleware/authMiddleware");
 
 router.get("/", userController.fetchAllUsers);
 router.post("/login", userController.login);
-router.post("/create", userController.addNewUser);
+router.post("/create", admin, userController.addNewUser);
 // router.post("/create", protect, admin, userController.addNewUser);
 router.put("/:id", protect, admin, userController.updateUser);
 router.delete("/:id", protect, admin, userController.deleteUser);
